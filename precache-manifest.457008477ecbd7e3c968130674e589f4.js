9e357075718020e30312b7147c4762d166718c89
jcook3195 <jcook3195@gmail.com>
2020-01-26 19:24:35 -0500

    Updates

---
+++ precache-manifest.457008477ecbd7e3c968130674e589f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d82f93a3182f52780c4964218696da",
+    "url": "/reactjs-quiz/index.html"
+  },
+  {
+    "revision": "81fbc2355a086720b049",
+    "url": "/reactjs-quiz/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "611827236ce7178d2185",
+    "url": "/reactjs-quiz/static/css/main.b1396d54.chunk.css"
+  },
+  {
+    "revision": "81fbc2355a086720b049",
+    "url": "/reactjs-quiz/static/js/2.e9521113.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactjs-quiz/static/js/2.e9521113.chunk.js.LICENSE"
+  },
+  {
+    "revision": "611827236ce7178d2185",
+    "url": "/reactjs-quiz/static/js/main.3d96d63e.chunk.js"
+  },
+  {
+    "revision": "de49c5737717d5664de7",
+    "url": "/reactjs-quiz/static/js/runtime-main.a46ab4f7.js"
+  }
+]);
